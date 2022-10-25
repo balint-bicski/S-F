e5ddf4c50b4465f814d58c40790e3a73fe90b79f
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2022-10-25 16:58:38 +0200

    Implement remaining ciff reader components

--- parser/src/ciffreader.cpp
+++ parser/src/ciffreader.cpp
@@ -62,6 +62,86 @@ bool CIFFReader::parse(std::istream& stream, u64 expected_size) {
     return true;
 }
 
-bool read_caption_and_tags(std::istream& stream, u64 expected_size) {
-    // TODO
+bool CIFFReader::read_caption_and_tags(std::istream& stream, u64 expected_size) {
+    std::string caption;
+    std::vector<std::string> tags;
+
+    /* Read caption char by char until the first '\n'. */
+    char c;
+    while (true) {
+        /* Assert that we can read the next character according to the expected size. */
+        if (caption.length() >= expected_size) {
+            return false;
+        }
+
+        /* Read and process the next character. */
+        stream.read(&c, 1);
+        if (!stream) {
+            return false;
+        }
+
+        if (c == '\n') {
+            break;
+        }
+
+        caption.push_back(c);
+    }
+
+    /* Read tags as well, tag by tag. */
+    u64 tag_bytes_read = 0;
+    while (stream && tag_bytes_read < expected_size - caption.length()) {
+        std::string current_tag;
+
+        /* Read and process the next tag, char by char. */
+        while (true) {
+            /* Assert that we can read the next character according to the expected size. */
+            if (tag_bytes_read >= expected_size - caption.length()) {
+                return false;
+            }
+
+            /* Read and process the next character. */
+            stream.read(&c, 1);
+            if (!stream || c == '\n') {
+                return false;
+            }
+            tag_bytes_read += 1;
+
+            /* On a delimiter, we save the current tag if it contains anything. */
+            if (c == '\0') {
+                if (current_tag.length() > 0) {
+                    tags.push_back(current_tag);
+                }
+                break;
+            }
+
+            current_tag.push_back(c);
+        }
+    }
+
+    /* We exit the outer loop when either the stream contains an error or we read enough bytes. */
+
+    /* Assert that we read exactly the correct amount of bytes in total. */
+    if (tag_bytes_read + caption.length() != expected_size || !stream) {
+        return false;
+    }
+
+    /* Save parsed data. */
+    this->data.caption = caption;
+    this->data.tags = tags;
+    return true;
+}
+
+bool CIFFReader::read_pixels(std::istream& stream, u64 expected_size) {
+    /* Preallocate a large enough buffer. */
+    u8 buffer[expected_size];
+
+    /* Read content into buffer. */
+    stream.read((char*) buffer, expected_size);
+    if (!stream) {
+        return false;
+    }
+
+    /* Save parsed data. */
+    this->data.pixels = buffer;
+    return true;
 }
