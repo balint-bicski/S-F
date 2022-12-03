89271173d77c377ed4cc7ee78ecaa11225f72c80
Gábor Beckl <becgabi@gmail.com>
2022-12-03 09:21:15 +0100

    Fix json format

--- parser/src/caff.cpp
+++ parser/src/caff.cpp
@@ -8,16 +8,16 @@ CAFF::CAFF() : is_preview_created(false), preview_location(NULL) {}
 
 void CAFF::print_metadata(std::ostream& stream, u8 prefix) const {
     std::string padding(prefix, ' ');
-    
+
     stream << padding << "{" << std::endl;
-    stream << padding << "  ciff_count: " << this->ciff_count << "," << std::endl;
+    stream << padding << "  \"ciff_count\": " << this->ciff_count << "," << std::endl;
     {
-        stream << padding << "  credits: ";
+        stream << padding << "  \"credits\": ";
         this->credits.print_metadata(stream, prefix + 2);
         stream << "," << std::endl;
     }
     {
-        stream << padding << "  frames: [" << std::endl;
+        stream << padding << "  \"frames\": [" << std::endl;
         for (const Frame& frame : this->frames) {
             frame.print_metadata(stream, prefix + 4);
 
@@ -29,11 +29,11 @@ void CAFF::print_metadata(std::ostream& stream, u8 prefix) const {
         stream << padding << "  ]," << std::endl;
     }
     {
-        stream << padding << "  preview_created: ";
+        stream << padding << "  \"preview_created\": ";
         stream << (this->is_preview_created ? "\"yes\"" : "\"no\"");
         if (this->is_preview_created) {
             stream << "," << std::endl;
-            stream << padding << "  preview_location: \"" << this->preview_location << "\"";
+            stream << padding << "  \"preview_location\": \"" << this->preview_location << "\"";
         }
         stream << std::endl;
     }
@@ -42,10 +42,10 @@ void CAFF::print_metadata(std::ostream& stream, u8 prefix) const {
 
 void Credits::print_metadata(std::ostream& stream, u8 prefix) const {
     std::string padding(prefix, ' ');
-    
+
     stream << "{" << std::endl;
-    stream << padding << "  creator: \"" << this->creator << "\"," << std::endl;
-    stream << padding << "  date: \"" << this->year << "-";
+    stream << padding << "  \"creator\": \"" << this->creator << "\"," << std::endl;
+    stream << padding << "  \"date\": \"" << this->year << "-";
     stream << std::setw(2) << std::setfill('0') << (u64) this->month << "-";
     stream << std::setw(2) << (u64) this->day << " ";
     stream << std::setw(2) << (u64) this->hour << ":";
@@ -55,26 +55,26 @@ void Credits::print_metadata(std::ostream& stream, u8 prefix) const {
 
 void Frame::print_metadata(std::ostream& stream, u8 prefix) const {
     std::string padding(prefix, ' ');
-    
+
     stream << padding << "{" << std::endl;
     {
-        stream << padding << "  image: ";
+        stream << padding << "  \"image\": ";
         this->image.print_metadata(stream, prefix + 2);
         stream << "," << std::endl;
     }
-    stream << padding << "  duration: " << this->duration << std::endl;
+    stream << padding << "  \"duration\": " << this->duration << std::endl;
     stream << padding << "}";
 }
 
 void CIFF::print_metadata(std::ostream& stream, u8 prefix) const {
     std::string padding(prefix, ' ');
-    
+
     stream << "{" << std::endl;
-    stream << padding << "  width: " << this->width << "," << std::endl;
-    stream << padding << "  height: " << this->height << "," << std::endl;
-    stream << padding << "  caption: \"" << this->caption << "\"," << std::endl;
+    stream << padding << "  \"width\": " << this->width << "," << std::endl;
+    stream << padding << "  \"height\": " << this->height << "," << std::endl;
+    stream << padding << "  \"caption\": \"" << this->caption << "\"," << std::endl;
     {
-        stream << padding << "  tags: [";
+        stream << padding << "  \"tags\": [";
         for (const std::string& tag : this->tags) {
             stream << "\"" << tag << "\"";
             if (&tag != &this->tags.back()) {
@@ -83,7 +83,7 @@ void CIFF::print_metadata(std::ostream& stream, u8 prefix) const {
         }
         stream << "]," << std::endl;
     }
-    stream << padding << "  content_size: " << this->pixels_size << std::endl;
+    stream << padding << "  \"content_size\": " << this->pixels_size << std::endl;
     stream << padding << "}";
 }
 
@@ -105,7 +105,7 @@ CIFF::CIFF(const CIFF& from) {
         this->pixels = new u8[from.pixels_size];
         memcpy((char*) this->pixels, (char*) from.pixels, from.pixels_size);
         this->pixels_size = from.pixels_size;
-    }    
+    }
 }
 
 CIFF::CIFF(CIFF&& from) {
