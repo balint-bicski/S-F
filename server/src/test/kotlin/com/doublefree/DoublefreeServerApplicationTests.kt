a20dd36d28458dd4aada3ad801c203c202824b44
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2022-12-04 17:24:42 +0100

    Create sample mockmvc test

--- server/src/test/kotlin/com/doublefree/DoublefreeServerApplicationTests.kt
+++ server/src/test/kotlin/com/doublefree/DoublefreeServerApplicationTests.kt
@@ -3,14 +3,19 @@ package com.doublefree
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
-class DoublefreeServerApplicationTests @Autowired constructor(
-	val client: WebTestClient
-) {
+@SpringBootTest
+@AutoConfigureMockMvc
+class DoublefreeServerApplicationTests {
+	@Autowired
+	lateinit var mockMvc: MockMvc
 
 	@Test
 	fun contextLoads() {
@@ -19,11 +24,10 @@ class DoublefreeServerApplicationTests @Autowired constructor(
 	//Without login
 	@Test
 	fun caffListWithoutAuthentication() {
-		client
-			.get().uri("/api/caff-files")
-			.exchange()
-			.expectStatus().isOk
-			.expectBody().json("[]")
+		mockMvc
+			.perform(get("/api/caff-files"))
+			.andExpect(status().isOk)
+			.andExpect(content().json("[]"))
 	}
 
 	@Test
