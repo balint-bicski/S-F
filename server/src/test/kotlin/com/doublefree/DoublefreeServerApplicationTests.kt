d0e2d07c411e3be266ec195d9b8f965bcec6d549
SillingerMester <sili98@sch.bme.hu>
2022-12-04 16:37:39 +0100

    Begin creating integration tests.

--- server/src/test/kotlin/com/doublefree/DoublefreeServerApplicationTests.kt
+++ server/src/test/kotlin/com/doublefree/DoublefreeServerApplicationTests.kt
@@ -1,13 +1,84 @@
 package com.doublefree
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest
-class DoublefreeServerApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class DoublefreeServerApplicationTests @Autowired constructor(
+	val client: WebTestClient
+) {
 
 	@Test
 	fun contextLoads() {
 	}
 
+	//Without login
+	@Test
+	fun caffListWithoutAuthentication() {
+		client
+			.get().uri("/api/caff-files")
+			.exchange()
+			.expectStatus().isOk
+			.expectBody().json("[]")
+	}
+
+	@Test
+	fun uploadCaffWithoutAuthentication(){
+
+	}
+
+	@Test
+	fun purchaseCaffWithoutAuthentication(){
+
+	}
+	@Test
+	fun downloadCaffWithoutAuthentication(){
+
+	}
+
+	@Test
+	fun commentWithoutAuthentication(){
+
+	}
+
+	@Test
+	fun deleteCommentWithoutAuthentication(){
+
+	}
+
+	//With login as user
+
+	//With login as admin
+
+	//Non-existing entities
+
+	@Test
+	fun getNonExistingCaffDetails(){
+
+	}
+
+	@Test
+	fun uploadAsNonExistingUser(){
+
+	}
+
+	@Test
+	fun commentAsNonExistingUser(){
+
+	}
+
+	//Malformed entities
+	@Test
+	fun uploadInvalidCaff(){
+
+	}
+
+	@Test
+	fun createEmptyComment(){
+
+	}
 }
